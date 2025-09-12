af9a4865738c374a46e1af7e8e7261c2e8a90963
Matthew Troutman <MST62@ou.edu>
2025-09-11 22:35:10 -0500

    Console Added

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name:Matthew Troutman
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -14,7 +14,31 @@ namespace HW1a_Sales
     internal class Program
     {
         static void Main(string[] args)
-        {
+       
+                 {
+                const double tax = 0.085;
+
+                Console.Write("What is the product name of the item you are purchasing? ");
+                string item = Console.ReadLine();
+
+                Console.Write("How many Football Ticket's do you want to buy? ");
+                int qty = int.Parse(Console.ReadLine());
+
+                Console.Write("What is the price for each Football Ticket? ");
+                double price = double.Parse(Console.ReadLine());
+
+                double subtotal = qty * price;
+                double salesTax = subtotal * tax;
+                double total = subtotal + salesTax;
+
+                Console.WriteLine();
+                Console.WriteLine("Subtotal: $" + subtotal);
+                Console.WriteLine("Sales Tax: $" + salesTax);
+                Console.WriteLine("Total: $" + total);
+
+                Console.ReadLine();
+            }
         }
     }
-}
+    
+
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -15,6 +15,34 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            Console.Write("What is your first name? ");
+            string first = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string last = Console.ReadLine();
+
+            Console.Write("What is your student ID? ");
+            string id = Console.ReadLine();
+
+            Console.Write("What is your overall percentage grade for Assigments? ");
+            double q = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is your overall percentage grade for Participation? ");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is your overall percentage grade for Exam 1? ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is your overall percentage grade for Exam 2? ");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is your overall percentage grade on the Final? ");
+            double i = Convert.ToDouble(Console.ReadLine());
+
+
+            double grade = (q * 0.2) + (p * 0.15) + (h * 0.15) + (e * 0.25) + (i * 0.25);
+
+            Console.WriteLine("Final Grade: " + grade + "%");
         }
     }
 }
